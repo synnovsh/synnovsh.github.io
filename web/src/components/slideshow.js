794b1280d6932728b4de6e28025def0d0aa1574c
Synnøve Soon Halle <synnovsh@stud.ntnu.no>
2019-09-26 21:42:41 +0200

    Minor edits to typography, spacing

--- web/src/components/slideshow.js
+++ web/src/components/slideshow.js
@@ -40,7 +40,7 @@ const Button = styled.button`
   ${props => props.disabled && "visibility: hidden;"}
   width: 50px;
   height: 50px;
-  background: rgba(0, 0, 0, 0.4);
+  background: rgba(255, 255, 255, 0.4);
   border: 0;
   outline: 0;
   padding: 0;
@@ -61,7 +61,7 @@ const Arrow = ({ direction }) => (
   >
     <path
       d="M24.291,14.276L14.705,4.69c-0.878-0.878-2.317-0.878-3.195,0l-0.8,0.8c-0.878,0.877-0.878,2.316,0,3.194  L18.024,16l-7.315,7.315c-0.878,0.878-0.878,2.317,0,3.194l0.8,0.8c0.878,0.879,2.317,0.879,3.195,0l9.586-9.587  c0.472-0.471,0.682-1.103,0.647-1.723C24.973,15.38,24.763,14.748,24.291,14.276z"
-      fill="white"
+      fill="black"
       fillOpacity="0.4"
     />
   </svg>
--- web/src/templates/project.js
+++ web/src/templates/project.js
@@ -29,24 +29,48 @@ export const query = graphql`
   }
 `
 
-const ProjectArticle = styled.article``
+const maxWidth = 800
+
+const ProjectArticle = styled.article`
+  display: grid;
+  grid-template-columns: minmax(25px, auto) minmax(0, ${maxWidth}px) minmax(
+      25px,
+      auto
+    );
+  grid-template-rows: auto;
+  width: 100%;
+`
 
 const ProjectInfo = styled.section`
+  grid-row: 1/2;
+  grid-column: 2/3;
+
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  div {
+    padding: 10px;
+  }
+  h1 {
+    text-transform: uppercase;
+  }
 `
 
 const BodySection = styled.section`
+  grid-row: 2/3;
+  grid-column: 1/4;
   display: grid;
-  grid-template-columns: minmax(25px, auto) minmax(0, 800px) minmax(25px, auto);
+  grid-template-columns: minmax(25px, auto) minmax(0, ${maxWidth}px) minmax(
+      25px,
+      auto
+    );
   width: 100%;
-
   padding-top: 50px;
 `
 
 const Content = styled.div`
   grid-area: 1/2;
-
   h1,
   h2,
   p {
@@ -54,7 +78,7 @@ const Content = styled.div`
   }
 
   video,
-  img {
+  figure {
     margin: 4em 0;
   }
 
@@ -85,13 +109,19 @@ const Content = styled.div`
     flex-flow: row wrap;
     width: 100%;
 
-    figcaption {
-      padding: 4em 0 0 1em;
-      flex: 1 0 70px;
-    }
-
     img {
       object-fit: scale-down;
+    }
+
+    figcaption {
+      padding: 1em 0 0 0;
+    }
+
+    @media screen and (min-width: 800px) {
+      figcaption {
+        padding: 0 0 0 1em;
+        flex: 1 0 10px;
+      }
     }
   }
 `
@@ -102,6 +132,7 @@ const ProjectTemplate = ({ data }) => {
   const { title, _rawBody: body, category, stack, live, code } = project
   return (
     <Layout>
+      <SEO title={title} />
       <ProjectArticle>
         <ProjectInfo>
           <div>
@@ -109,13 +140,8 @@ const ProjectTemplate = ({ data }) => {
           </div>
           <div>
             {category && category}
-            {stack && (
-              <ul>
-                {stack.map(s => (
-                  <li key={s}>{s}</li>
-                ))}
-              </ul>
-            )}
+            {stack &&
+              stack.map((item, index) => `${(index ? ", " : "") + item}`)}
             {code && code}
             {live && live}
           </div>
